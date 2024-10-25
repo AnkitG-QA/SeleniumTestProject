70c49319b9fae9236e1a1dbdf9092a21600a084a
AnkitG-QA <gangurdeankit19@gmail.com>
2024-10-25 20:19:50 +0530

    function code commit

---
+++ src/main/java/Functions/Fun001.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class Fun001 {
+
+    public static int add(){
+        int a=4,b=5,c;
+        c=a+b;
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println("The addition is: "+add());
+    }
+}
